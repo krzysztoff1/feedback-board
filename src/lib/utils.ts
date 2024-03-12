cdbfd6cb7e50f8dd5e9c10281eb3ddf381f2d6bb
Krzysztof <halidu13@gmail.com>
2024-03-12 20:50:55 +0100

    fix: fallback if navigator is not defined

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getUserLocale() {
-  return navigator.language;
+  return typeof window !== "undefined" ? navigator.language : "en-US";
 }
 
 export function getRelativeTimeString(
