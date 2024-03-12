4d26d4aa0bd29b9bbac2c1cd67839485139ca7c1
Krzysztof <halidu13@gmail.com>
2024-03-12 21:42:22 +0100

    fix: disable login until signed in

--- src/app/_components/auth/auth-form.tsx
+++ src/app/_components/auth/auth-form.tsx
@@ -2,22 +2,34 @@
 
 import { type getProviders, signIn } from "next-auth/react";
 import { Button } from "~/components/ui/button";
-import { memo } from "react";
+import { memo, useState } from "react";
 
 interface AuthFormProps {
   providers: Awaited<ReturnType<typeof getProviders>>;
 }
 
 export const AuthForm = memo(({ providers }: AuthFormProps) => {
+  const [currentlSignedIn, setCurrentlSignedIn] = useState<string | null>(null);
+
   return (
     <div className="flex flex-col gap-4">
       {Object.values(providers ?? {}).map((provider) => (
         <Button
           key={provider.id}
-          onClick={() => signIn(provider.id)}
+          onClick={() => {
+            setCurrentlSignedIn(provider.id);
+            signIn(provider.id);
+          }}
           variant={"default"}
+          disabled={currentlSignedIn !== null}
         >
-          Sign in with <strong className="ml-1">{provider.name}</strong>
+          {currentlSignedIn === provider.id ? (
+            "Signing in..."
+          ) : (
+            <>
+              Sign in with <strong className="ml-1">{provider.name}</strong>
+            </>
+          )}
         </Button>
       ))}
     </div>
