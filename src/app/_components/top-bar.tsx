fa1a3e8f0d263649a8ba5a64f8ed42db32cb96aa
Krzysztof <halidu13@gmail.com>
2024-03-15 22:00:01 +0100

    feat: landing

--- src/app/_components/top-bar.tsx
+++ src/app/_components/top-bar.tsx
@@ -1,11 +1,14 @@
 import { memo } from "react";
 import { SignOutButton } from "./auth/sign-out-button";
+import Link from "next/link";
 
 export const TopBar = memo(() => {
   return (
     <header className="sticky top-0 flex h-16 w-screen">
       <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        <h1 className="text-fore text-2xl font-bold">Feedback Board</h1>
+        <Link href="/">
+          <h1 className="text-fore text-2xl font-bold">Feedback Board</h1>
+        </Link>
         <div className="flex items-center space-x-4">
           <SignOutButton />
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { unstable_noStore as noStore } from "next/cache";
-import { getProviders } from "next-auth/react";
-import { AuthForm } from "./_components/auth/auth-form";
 import { headers } from "next/headers";
 import { getServerAuthSession } from "~/server/auth";
 import { redirect } from "next/navigation";
 import { api } from "~/trpc/server";
 import { PublicBoard } from "~/components/public-board/PublicBoard";
 import { SITE_URL } from "~/lib/constants";
+import { Hero } from "./landing/hero";
 
 export default async function Home() {
   noStore();
@@ -41,26 +40,9 @@ export default async function Home() {
     redirect(SITE_URL);
   }
 
-  const providers = await getProviders();
-
-  if (session) {
-    redirect("/dashboard");
-  }
-
   return (
-    <main className="grid min-h-screen place-content-center">
-      <section className="mb-8">
-        <h1 className="text-center text-3xl font-bold">Feedback Board</h1>
-      </section>
-
-      <AuthForm
-        providers={providers}
-        signInCallbackSearchParams={{
-          isSubdomain: String(isSubdomain),
-          callback: String(true),
-          hostName,
-        }}
-      />
+    <main className="flex h-screen flex-col items-center justify-start">
+      <Hero />
     </main>
   );
 }
