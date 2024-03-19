0593c85032d78f442dc733370c62f980f60e73c4
Krzysztof <halidu13@gmail.com>
2024-03-19 12:40:20 +0100

    fix: lowercase filename

--- src/components/auth/auth-form.tsx
+++ src/components/auth/auth-form.tsx
@@ -54,7 +54,7 @@ export const AuthForm = memo(
             ) : (
               <span className="flex items-center">
                 <Image
-                  src={`/providers/${provider.name}.svg`}
+                  src={`/providers/${provider.name.toLowerCase()}.svg`}
                   alt={`${provider.name} icon`}
                   width={24}
                   height={24}
