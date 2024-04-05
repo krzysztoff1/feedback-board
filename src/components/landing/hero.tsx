a5237fe272183cdf78ffabced985159f6122d5ea
Krzysztof <halidu13@gmail.com>
2024-04-05 11:04:20 +0200

    fix: layout

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -33,29 +33,29 @@ export const Hero = memo(() => {
       <Spotlight fill="white" />
 
       <motion.div
-        className="flex h-[75vh] flex-col items-center justify-center px-4"
+        className="flex h-[75vh] flex-col justify-center px-4 sm:items-center"
         variants={heroVariants}
         animate="animate"
         initial="hidden"
       >
         <motion.h1
-          className="relative z-10 max-w-xl text-balance bg-gradient-to-b from-neutral-200 to-neutral-600  bg-clip-text text-center font-sans text-xl  font-bold text-transparent md:text-7xl"
+          className="relative z-10 max-w-xl text-balance bg-gradient-to-b from-neutral-200 to-neutral-600 bg-clip-text font-sans text-4xl font-bold text-transparent sm:text-center md:text-7xl"
           variants={heroVariants}
         >
           Feedback-driven product development
         </motion.h1>
         <motion.p
-          className="mt-4 max-w-xl text-balance text-center text-foreground/80"
+          className="mt-4 max-w-xl text-balance text-foreground/80 sm:text-center"
           variants={heroVariants}
         >
           {`Prioritize what to build next. Listen to your users. Keep track of what you're working on. Share your roadmap with the world.`}
         </motion.p>
 
         <motion.div
-          className="mt-8 flex flex-col items-center space-y-4"
+          className="mt-8 flex flex-col space-y-4 sm:items-center"
           variants={heroVariants}
         >
-          <Button variant={"default"} className="px-8" asChild>
+          <Button variant={"default"} className="max-w-xs px-8" asChild>
             <Link href="/auth/signin">Get started</Link>
           </Button>
           <Link
